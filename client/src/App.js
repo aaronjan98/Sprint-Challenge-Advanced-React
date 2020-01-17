700d9d9bd95fbcc138d26436a0e45dbb0a1b8e5a
aaronjan <aaronjan98@gmail.com>
2020-01-17 08:44:54 -0800

    moved data.js to client folder and iterated over it

--- client/src/App.js
+++ client/src/App.js
@@ -1,26 +1,25 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
+import Data from './data';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+
+  render (){
+    return (
+      <div className="App">
+        { Data.data.map((individual, index) => {
+            return (
+              <div key={index}>
+                <h4>{individual.name}</h4>
+                <p>Country: {individual.country}</p>
+                <p>Searches: {individual.searches}</p>
+              </div>
+            );
+          })
+        }
+      </div>
+    );
+  }
 }
 
 export default App;
